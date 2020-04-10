5c206071b596ab235d83d7ee3582a690e70c2458
Brik000 <35845838+Brik000@users.noreply.github.com>
2020-04-09 23:39:11 -0500

    minor changes

--- AppExperto2020/app/src/main/java/com/example/appexperto2020/view/CreateUserActivity.java
+++ AppExperto2020/app/src/main/java/com/example/appexperto2020/view/CreateUserActivity.java
@@ -1,14 +1,20 @@
 package com.example.appexperto2020.view;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.example.appexperto2020.R;
 import com.example.appexperto2020.util.MultiSelectionSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateUserActivity extends AppCompatActivity {
 
@@ -24,7 +30,12 @@ public class CreateUserActivity extends AppCompatActivity {
 
     private Button btnNewUser;
 
-    private MultiSelectionSpinner spinnerIntereses;
+    Spinner spinnerIntereses;
+
+
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +48,15 @@ public class CreateUserActivity extends AppCompatActivity {
         txtRepeatPassword=findViewById(R.id.editTextRepeatPassword);
         txtDescription=findViewById(R.id.editTextDescriptionNewUser);
         btnNewUser=findViewById(R.id.butNewUser);
-        spinnerIntereses=findViewById(R.id.inputIterests);
+        spinnerIntereses=(Spinner)findViewById(R.id.inputIterests);
+
+        //TODO
+        //get interests from database
+        List<String> list = new ArrayList<String>();
+        list.add("List1");
+        list.add("List2");
+        spinnerIntereses.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list));
     }
     @Override
     public void onBackPressed() {
